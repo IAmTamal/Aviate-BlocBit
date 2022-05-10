c30f23917cfbc0ec7d61bb6aa27fc98199cbc9f5
Tamal Das <gyansujan69@gmail.com>
2022-05-10 16:17:57 +0530

    Updated footer.

--- src/components/BitcoinHome.jsx
+++ src/components/BitcoinHome.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import Navbar from "./Navbar"
 import "../styles/HomeContainer.css"
-import blockchainimg from "../assets/blocks1.svg"
+import blockchainimg from "../assets/bit1.svg"
 import "../styles/BitcoinHome.css"
+import Footer from './Footer'
 
 
 const BitcoinHome = () => {
@@ -15,7 +16,7 @@ const BitcoinHome = () => {
                 <div className="col-xxl-6 col-xl-6 col-lg-6 col-md-12">
                     <div className="home_imgdiv">
 
-                        <img src={blockchainimg} alt="" className='blockchain_img' />
+                        <img src={blockchainimg} alt="" className='bitcoinimg' />
                     </div>
                 </div>
 
@@ -25,17 +26,17 @@ const BitcoinHome = () => {
 
                     <div className="home_textdiv">
                         <p className='textdiv_welcome'>Get started <br /> with Bitcoins</p>
-
+                        <br />
 
                         <p className='textdiv_ultimate'>Bitcoin uses peer-to-peer technology to operate with no central authority or banks; managing transactions and the issuing of bitcoins is carried out collectively by the network.
 
                             <br /><br />
 
-                            Bitcoin is open-source; its design is public, nobody owns or controls Bitcoin and everyone can take part.
+                            <p className='textdiv_tryour'>Read and learn more about bitcoins</p>
 
-                            <br /> <br />
+                            <button className='btn btn-warning currexbtn'>Bitcoins  </button>
 
-                            Through many of its unique properties, Bitcoin allows exciting uses that could not be covered by any previous payment system.
+
 
 
                         </p>
@@ -51,6 +52,9 @@ const BitcoinHome = () => {
 
 
             </div>
+
+
+
 
         </>
     )
