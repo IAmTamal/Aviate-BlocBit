3ad9ed64d77da0fd9eaa37da9cd091f3fb13ca4e
Tamal Das <gyansujan69@gmail.com>
2022-05-11 01:03:05 +0530

    Updated blockchain info page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Navbar from './components/Navbar'
 import AboutUs from './pages/AboutUs'
+import BlockchainInfo from './pages/BlockchainInfo'
 import ContactUs from './pages/ContactUs'
 import CurrencyConverter from './pages/CurrencyConverter'
 import LandingPage from './pages/LandingPage'
@@ -19,6 +20,9 @@ const App = () => {
                     <Route path="/about" element={<AboutUs />} />
                     <Route path="/exchange" element={<CurrencyConverter />} />
                     <Route path="/contactus" element={<ContactUs />} />
+
+
+                    <Route path="/blockchain-info" element={<BlockchainInfo />} />
 
                 </Routes>
             </Router>
