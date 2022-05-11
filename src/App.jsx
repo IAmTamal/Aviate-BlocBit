10fc7735bec4f44bc90dd40546f3dc8a698d84e0
Tamal Das <gyansujan69@gmail.com>
2022-05-11 10:46:25 +0530

    Added Bitcoin pages. (#6)

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Navbar from './components/Navbar'
 import AboutUs from './pages/AboutUs'
+import BitcoinInfo from './pages/BitcoinInfo'
 import BlockchainInfo from './pages/BlockchainInfo'
 import ContactUs from './pages/ContactUs'
 import CurrencyConverter from './pages/CurrencyConverter'
@@ -23,6 +24,7 @@ const App = () => {
 
 
                     <Route path="/blockchain-info" element={<BlockchainInfo />} />
+                    <Route path="/bitcoin-info" element={<BitcoinInfo />} />
 
                 </Routes>
             </Router>
