cd0c17de4b3e84fece224785c847b97e8bbe2bb1
Tamal Das <gyansujan69@gmail.com>
2022-05-13 14:32:12 +0530

    auth : 1 (#10)
    * Fixed calculator
    
    * Fixed responsiveness, added about us.
    
    * Auth : 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,48 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Navbar from './components/Navbar'
 import AboutUs from './pages/AboutUs'
 import BitcoinInfo from './pages/BitcoinInfo'
 import BlockchainInfo from './pages/BlockchainInfo'
 import ContactUs from './pages/ContactUs'
 import CurrencyConverter from './pages/CurrencyConverter'
 import LandingPage from './pages/LandingPage'
+import { Auth0Provider } from "@auth0/auth0-react";
+
 
 const App = () => {
+    const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+    const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
     return (
         <>
+            <Auth0Provider
+                domain={domain}
+                clientId={clientId}
+                redirectUri={window.location.origin}
+                useRefreshTokens
+                cacheLocation="localstorage"
+            >
 
-            <Router>
-                <Routes>
+                <Router>
+                    <Routes>
 
 
 
-                    <Route path="/" element={<LandingPage />} />
-                    <Route path="/about" element={<AboutUs />} />
-                    <Route path="/exchange" element={<CurrencyConverter />} />
-                    <Route path="/contactus" element={<ContactUs />} />
+                        <Route path="/" element={<LandingPage />} />
+                        <Route path="/about" element={<AboutUs />} />
+                        <Route path="/exchange" element={<CurrencyConverter />} />
+                        <Route path="/contactus" element={<ContactUs />} />
 
 
-                    <Route path="/blockchain-info" element={<BlockchainInfo />} />
-                    <Route path="/bitcoin-info" element={<BitcoinInfo />} />
+                        <Route path="/blockchain-info" element={<BlockchainInfo />} />
+                        <Route path="/bitcoin-info" element={<BitcoinInfo />} />
 
-                    <Route path="/currencyexchange" element={<CurrencyConverter />} />
+                        <Route path="/currencyexchange" element={<CurrencyConverter />} />
 
-                </Routes>
-            </Router>
+                    </Routes>
+
+                </Router>
+
+            </Auth0Provider>
 
 
         </>
